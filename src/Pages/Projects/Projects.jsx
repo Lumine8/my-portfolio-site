41966b8bf32010ac384daa1982255b2c47ea0fbe
Lumine8 <sankargopan1@gmail.com>
2023-07-31 22:19:36 +0530

    ...

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,21 +1,14 @@
+import { v4 as uuidv4 } from "uuid";
+
 import NavLinks from "../../Nav/NavLinks";
 import "./Projects.scss";
 import cartProject from "../../assets/ProjectsPicks/Panda-Cart.png";
+import projec from "../../assets/ProjectsPicks/projec.png";
 import socialMedia from "../../assets/ProjectsPicks/React-App.png";
-import { v4 as uuidv4 } from "uuid";
 import ProjCard from "./ProjectsCard/ProjectCard";
 
 export default function Projects() {
   const projectsListing = [
-    {
-      _id: uuidv4(),
-      title: "Panda Cart",
-      subtitle: "E-Commerce Web-App",
-      desc: "This is an e-commerce web application to purchase food products. The app features a simple user interface and a diverse range of products.",
-      website: "https://panda-cart.vercel.app/",
-      github: "https://github.com/Lumine8/ecommerce-app",
-      img: cartProject,
-    },
     {
       _id: uuidv4(),
       title: "Lucky Panda",
@@ -24,6 +17,15 @@ export default function Projects() {
       website: "https://lucky-panda.vercel.app/signin",
       github: "https://github.com/Lumine8/lucky-panda",
       img: socialMedia,
+    },
+    {
+      _id: uuidv4(),
+      title: "Panda Cart",
+      subtitle: "E-Commerce Web-App",
+      desc: "This is an e-commerce web application to purchase food products. The app features a simple user interface and a diverse range of products.",
+      website: "https://panda-cart.vercel.app/",
+      github: "https://github.com/Lumine8/ecommerce-app",
+      img: cartProject,
     },
     // {_id:uuidv4(),
     //   title: "",
@@ -37,7 +39,11 @@ export default function Projects() {
     <div className="container">
       <NavLinks />
       <div>
-        <h1>Check Out My Projects.</h1>
+        <h1>
+          {" "}
+          <img src={projec} alt="projects" className="project-h1-img" />
+          Projects.
+        </h1>
       </div>
       <div className="Projects-Container">
         <ul>
@@ -45,7 +51,9 @@ export default function Projects() {
             const { _id, title, subtitle, desc, website, github, img } = item;
             return (
               <li key={_id}>
-                <ProjCard props={{ title, subtitle, desc, website, github,img }} />
+                <ProjCard
+                  props={{ title, subtitle, desc, website, github, img }}
+                />
               </li>
             );
           })}
