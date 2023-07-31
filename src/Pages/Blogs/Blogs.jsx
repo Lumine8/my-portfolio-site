f9919f51945d5d786ff554a0ab9ca43b7f09c222
Lumine8 <sankargopan1@gmail.com>
2023-07-31 22:32:52 +0530

    ....

--- src/Pages/Blogs/Blogs.jsx
+++ src/Pages/Blogs/Blogs.jsx
@@ -5,7 +5,10 @@ export default function Blogs() {
     <div className="container">
       <NavLinks />
       <div>
-        <h1>Check Out My Blogs.</h1>
+        <h1>Blogs.</h1>
+      </div>
+      <div className="Blogs-container">
+        <h1>Nothing to see here...yet</h1>
       </div>
     </div>
   );
